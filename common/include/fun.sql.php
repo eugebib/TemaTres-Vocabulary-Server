48c3030e3f9d162716a9d8546d173246d2d95978
eugebib <eugebib@gmail.com>
2016-10-24 14:39:39 -0300

    :(

--- common/include/fun.sql.php
+++ common/include/fun.sql.php
@@ -196,9 +196,8 @@ function SQLbuscaSimple($texto){
 	left join $DBCFG[DBprefix]indice i on i.tema_id=tema.tema_id
 	left join $DBCFG[DBprefix]values v on v.value_id = relaciones.rel_rel_id
 	where
-	(tema.tema like ?
-	or tema.code like '%$texto%'
-	$where)
+	tema.tema like ?
+	$where
 	group by id_definitivo
 	order by rank desc,lower(tema.tema)",array($texto,"%$texto%"));
 
