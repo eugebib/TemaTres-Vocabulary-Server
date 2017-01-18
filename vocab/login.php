aed4a74ae7681425560b5726e831538cacdba213
eugebib <eugebib@gmail.com>
2017-01-18 11:20:09 -0300

    fix(footer): add function

--- vocab/login.php
+++ vocab/login.php
@@ -103,29 +103,7 @@
  				}// if session
 			?>
 		</div><!-- /.container -->
-		<div id="footer" class="footer">
-		  	<div class="container">
-		    	<?php
-					if (!$_GET["letra"]) {
-						echo HTMLlistaAlfabeticaUnica();
-					}
-				?>
-				<p class="navbar-text pull-left">
-					<?php
-						//are enable SPARQL
-						if (CFG_ENABLE_SPARQL==1) {
-							echo '<a class="label label-info" href="'.URL_BASE.'sparql.php" title="'.LABEL_SPARQLEndpoint.'">'.LABEL_SPARQLEndpoint.'</a>';
-						}
-						if (CFG_SIMPLE_WEB_SERVICE==1) {
-							echo '  <a class="label label-info" href="'.URL_BASE.'services.php" title="API"><span class="glyphicon glyphicon-share"></span> API</a>';
-						}
-						echo '  <a class="label label-info" href="'.URL_BASE.'xml.php?rss=true" title="RSS"><span class="icon icon-rss"></span> RSS</a>';
-						echo '  <a class="label label-info" href="'.URL_BASE.'index.php?s=n" title="'.ucfirst(LABEL_showNewsTerm).'"><span class="glyphicon glyphicon-fire"></span> '.ucfirst(LABEL_showNewsTerm).'</a>';
-					?>
-				</p>
-				<?php echo doMenuLang($metadata["arraydata"]["tema_id"]); ?>
-		  	</div>
-		</div>
+		<?php echo footer(); ?>
 		<?php echo HTMLjsInclude();?>
 	    <script>
 		    $(".toggle").on("click", function() {
