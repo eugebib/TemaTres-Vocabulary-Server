738324b66317593799ad128fe352c4a519aa381e
eugenia <eugebib@gmail.com>
2017-03-16 10:48:11 -0300

    fix(footer): hide lang

--- common/include/fun.html.php
+++ common/include/fun.html.php
@@ -2252,8 +2252,8 @@ function HTMLextraDataHeader($CFG)
 	return '<img src="logo.png" height="50px" alt="'.$CFG["HEADER_EXTRA"]["LINK_TITLE"].'">';
 }
 
-function footer() {
-
+function footer()
+{
 	global $DBCFG;
 
 	$lang = doMenuLang($metadata["arraydata"]["tema_id"]);
@@ -2280,8 +2280,7 @@ function footer() {
 	            <div class="col-sm-5 col-sm-offset-1 col-md-3 col-md-offset-3">
 	                <ul class="menu nav">
 	                	' . $contact .
-	                	$help .
-	                	$lang . '
+	                	$help .'
                     </ul>
 	            </div>
 	        </div>
