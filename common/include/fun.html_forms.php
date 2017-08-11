b1cdb3412d17865e9a93377788e94291539a85e6
eugebib <eugebib@gmail.com>
2017-08-11 13:34:15 -0300

    fix(xsearch)

--- common/include/fun.html_forms.php
+++ common/include/fun.html_forms.php
@@ -684,7 +684,7 @@ function HTMLformAdvancedSearch($array)
 		</form>
 		<div class="push"></div>';
 
-	if ($_GET[boton]==LABEL_Enviar) {
+	if ($_GET[boton]==LABEL_Buscar) {
 		$rows.=HTMLadvancedSearchResult($array);
 	}
 
