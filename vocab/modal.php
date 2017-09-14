2a29945b57ba6e58ea9274ba820140f4f826ddbf
eugebib <eugebib@gmail.com>
2017-09-14 13:01:32 -0300

    fix(modal)

--- vocab/modal.php
+++ vocab/modal.php
@@ -15,8 +15,8 @@ $metadata["arraydata"] = ($tterm_id>0) ? ARRAYtargetTerm($metadata["arraydata"][
 
     <head>
         <?= HTMLheader($metadata) ?>
-        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-        <?= '<script src="'.T3_WEBPATH.'bootstrap/js/bootstrap.min.js"></script>' ?>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
 
     <body>
