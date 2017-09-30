00567a78cf6ab9fe4b63a7963c6f30a68a11b185
eugenia <eugebib@gmail.com>
2017-09-30 14:14:29 -0300

    fix bug in ABM terminological sub-type relations

--- common/include/fun.admin.php
+++ common/include/fun.admin.php
@@ -2764,30 +2764,23 @@ function abm_rel_rel($do,$rel_id,$rel_type_id)
 	GLOBAL $DBCFG;
 
 	//sanitize
-	$rel_id=secure_data($rel_id,"int");
-	$rel_type_id=secure_data($rel_type_id,"int");
+	$rel_id      =secure_data($rel_id,"int");
+	$rel_type_id =secure_data($rel_type_id,"int");
+	$userId      =$_SESSION[$_SESSION["CFGURL"]]["ssuser_id"];
 
-	$userId=$_SESSION[$_SESSION["CFGURL"]]["ssuser_id"];
-
-	switch ($do)
-		{
+	switch ($do) {
 		case 'ALTA':
 			//check if the type relation exist
 			$ARRAYdataRelation=ARRAYdataRelation($rel_id);
-
 			$ARRAYtypeRelations=ARRAYtypeRelations($ARRAYdataRelation["t_relacion"],$rel_type_id);
-
-			if(count($ARRAYtypeRelations["$ARRAYdataRelation[t_relacion]"])=='1') {
-
+			if (count($ARRAYtypeRelations["$ARRAYdataRelation[t_relacion]"])=='1') {
 				$sql=SQL("update"," $DBCFG[DBprefix]tabla_rel set rel_rel_id='$rel_type_id', cuando=now(),uid='$userId' where id='$rel_id'");
 			}
-
-		break;
+			break;
 
 		case 'BAJA':
-
-			$sql=SQL("update"," $DBCFG[DBprefix]tabla_rel set rel_rel_id='NULL', cuando=now(),uid='$userId' where id='$rel_id'");
-		break;
+			$sql=SQL("update"," $DBCFG[DBprefix]tabla_rel set rel_rel_id=NULL, cuando=now(),uid='$userId' where id='$rel_id'");
+			break;
 
 		default:
 	}
