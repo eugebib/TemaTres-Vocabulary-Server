0f77ec1fdb4bbd4f9c19eb3fbbdb956f9a443b9a
Eugebib <eugebib@gmail.com>
2018-04-10 21:26:59 -0300

    style(menu): add vocabulary icons & colors

--- views/menu-local.view.php
+++ views/menu-local.view.php
@@ -3,6 +3,10 @@
 
     <head>
         <link href="common/css/local.css" rel="stylesheet">
+        <link href="common/css/icono-arg.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy|Work+Sans|Open+Sans|Roboto" rel="stylesheet">
+
+        <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
     </head>
 
     <body>
@@ -15,7 +19,8 @@
             <div class="boxes">
 
                 <?php foreach ($vocabularies as $k => $v) : ?>
-                    <a href="<?= $CFG['URL'] . $k ?>" class="box">
+                    <a href="<?= $CFG['URL'] . $k ?>" class="box <?= $v['style'] ?>">
+                        <i class="<?= $v['icon'] ?>"></i>
                         <?= $v['title'] ?>
                     </a>
                 <?php endforeach; ?>
